4654e6afedcec10d773322d412c516de3a724560
Matheus Carvalho Oliveira <mco.matheus@gmail.com>
2022-05-18 16:06:24 -0300

    teste dia_18/05

--- Python/analise.py
+++ Python/analise.py
@@ -1,7 +1,7 @@
 class analise:
     n = input("digite algo: ")
     print('É do tipo: ', type(n))
-    print('É numérico: {}'.format(n.isnumeric()), end=" ")
+    print('É numérico: {}'.format(n.isnumeric()), end="\n")
     print('É alfa númerico {}'.format(n.isalnum()))
     print('Está em maiúsculo: {}'.format(n.isupper()))
     print('É alfabético {}'.format(n.isalpha()))
