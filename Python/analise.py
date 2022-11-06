85feb6f224026300c0e27c93f391c4f9bc908186
Matheus Carvalho Oliveira <mco.matheus@gmail.com>
2022-11-06 16:59:39 -0300

    att 06/11
    Aplicando POO, class analise
    Get e Set, self = this, __init__ = construtor

--- Python/analise.py
+++ Python/analise.py
@@ -1,5 +1,20 @@
 class analise:
-    n = input("digite algo: ")
+    _n = ''
+    def get_n(self):
+        return self._n
+    
+    def set_n(self, dado):
+        if(dado == ''):
+            print('Digite algo para ser analisado!')
+        else:
+            self._n = dado
+
+    def __init__(self, dado) -> None:
+        self._n = dado
+
+    if(_n == ''):    
+        n = input("digite algo: ")
+
     print('É do tipo: ', type(n))
     print('É numérico: {}'.format(n.isnumeric()), end="\n")
     print('É alfa númerico {}'.format(n.isalnum()))
